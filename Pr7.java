65b4f29eda425e6c9d37736e2aa644fe8ed612f1
Khevna134 <khevnaraval134@gmail.com>
2026-05-01 11:20:49 +0530

    Pr7.java

---
+++ Pr7.java
@@ -0,0 +1,63 @@
+public class pr7 {
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0, 2, 0, 6, 0},
+            {2, 0, 3, 8, 5},
+            {0, 3, 0, 0, 7},
+            {6, 8, 0, 0, 9},
+            {0, 5, 7, 9, 0}
+        };
+
+        primMST(graph);
+    }
+
+    static void primMST(int[][] graph) {
+        int V = graph.length;
+        int[] parent = new int[V];
+        int[] key = new int[V];
+        boolean[] mstSet = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            key[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+
+        key[0] = 0;
+        parent[0] = -1;
+
+        for (int count = 0; count < V - 1; count++) {
+            int u = minKey(key, mstSet);
+            mstSet[u] = true;
+
+            for (int v = 0; v < V; v++) {
+                if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v]) {
+                    parent[v] = u;
+                    key[v] = graph[u][v];
+                }
+            }
+        }
+
+        printMST(parent, graph);
+    }
+
+    static int minKey(int[] key, boolean[] mstSet) {
+        int min = Integer.MAX_VALUE, minIndex = -1;
+
+        for (int v = 0; v < key.length; v++) {
+            if (!mstSet[v] && key[v] < min) {
+                min = key[v];
+                minIndex = v;
+            }
+        }
+        return minIndex;
+    }
+
+    static void printMST(int[] parent, int[][] graph) {
+        int total = 0;
+        for (int i = 1; i < graph.length; i++) {
+            System.out.println(parent[i] + " - " + i + " : " + graph[i][parent[i]]);
+            total += graph[i][parent[i]];
+        }
+        System.out.println("Total Weight: " + total);
+    }
+}

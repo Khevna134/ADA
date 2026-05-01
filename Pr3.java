55fb7f28413b41c60f40f79c50a65ced7160ddb2
Khevna134 <khevnaraval134@gmail.com>
2026-05-01 11:16:56 +0530

    Pr3.java

---
+++ Pr3.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class pr3 {
+    public static void main(String[] args) {
+        int n = 10000;
+        int[] arr = new int[n];
+        Random r = new Random();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = r.nextInt(100000);
+        }
+
+        long start = System.nanoTime();
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr
